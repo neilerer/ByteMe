c6cfa6a0b7a71b56fbd96a10240ac60b55d40520
cojowy <16202597@ucdconnect.ie>
2017-06-23 17:38:45 +0100

    removing unused files in django project

--- dublin_bus_project/home/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class RouteForm(forms.Form):
-    route=forms.CharField(max_length=10)
--- dublin_bus_project/home/test.py
+++
@@ -1,6 +0,0 @@
-import json
-
-with open("stops_on_routes.json") as file:
-    stops_on_routes=json.load(file)
-
-print(stops_on_routes)
