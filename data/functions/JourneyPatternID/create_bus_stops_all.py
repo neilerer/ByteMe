bb0b8d966f351fb7e2b3f6ce151b22a14b8ad0ec
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-07-13 22:10:35 +0100

    started, but decided to wait until the morning

---
+++ data/functions/JourneyPatternID/create_bus_stops_all.py
@@ -0,0 +1,22 @@
+# imports
+import os
+import glob
+
+
+# directory functions
+def jpif_to_jpid():
+	os.chdir("../../")
+	os.chdir("data/JourneyPatternID")
+
+def jpid_to_jpif():
+	os.chdir("../../")
+	os.chdir("functions/JourneyPatternID")
+
+
+# 
+def create_files():
+	# change directory
+	os.chdir("../../")
+	os.chdir("data/JourneyPatternID/")
+	# 
+	
