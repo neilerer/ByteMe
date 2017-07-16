d7e957353a622b7f6e9b8a8bb99ac226a7861bac
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-07-16 10:09:18 +0100

    testing a bus stop coordinate function

--- models/running_average/at_stop.py
+++ models/running_average/at_stop.py
@@ -4,7 +4,7 @@ import glob
 import general
 
 
-def stop_coordinates_all(file_name):
+def bust_stop_all_coordinates(file_name):
 	# data
 	headers = general.headers_list(file_name)
 	bus_stops_by_day = general.bus_stops_all(file_name)
@@ -12,16 +12,27 @@ def stop_coordinates_all(file_name):
 	# skip the first line
 	source.readline()
 	# coordinates
-	coordinates = []
+	coordinates = [{} for day in bus_stops_by_day]
 	for line in source:
+		# create the information to add to coordinates
 		line_list = line.strip().split(",")
+		sid = line_list[headers.index("StopID")]
 		lat = line_list[headers.index("Latitude")]
 		lon = line_list[headers.index("Longitude")]
-		coordinates.append((lat, lon))
+		# add the information to coordinates
+		weekday = lon = line_list[headers.index("WeekDay")]
+		temp_dict = coordinates[weekday]
+		if sid in temp_dict:
+			temp_dict[sid].append((lat, lon))
+		else:
+			temp_dict[sid] = [(lat, lon)]
 	source.close()
 	return coordinates
 
-
+coordinates = bust_stop_all_coordinates("00010001.csv")
+for item in coordinates:
+	print("")
+	print(item)
 
 # 	0	1	  2		3	4		5		6		7		8		9		10			11				12			13				14		15			16		17		  18		19		20		30		31		32				33			34				35			 	36				37			38					39				40			 41		42	
 # Year,Month,Day,Hours,Minute,Second,WeekDay,YearDay,Timestamp,LineID,Direction,JourneyPatternID,TimeFrame,VehicleJourneyID,Operator,Congestion,Longitude,Latitude,BusDelay,BlockID,VehicleID,StopID,AtStop,Temperature (C),Dew Point (C),Humidity (%),Pressure (hPa),Visibility (km),Wind Direction,Wind Speed (km/h),Gust Speed (km/h),Precipitation,Events,Conditions
