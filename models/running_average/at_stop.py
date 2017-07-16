3b354cb5d60be30f05892ec5b7858ef2f40d36b7
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-07-16 16:05:05 +0100

    evaluation

--- models/running_average/at_stop.py
+++ models/running_average/at_stop.py
@@ -57,32 +57,40 @@ def bus_stop_all_coordinates_at_stop(file_name):
 	return coordinates
 
 
+all_positions = bus_stop_all_coordinates("00010001.csv")
+at_stop = bus_stop_all_coordinates_at_stop("00010001.csv")
 
-coordinates = bus_stop_all_coordinates_at_stop("00010001.csv")
-monday = coordinates[0]
-monday_226 = monday['226']
-# starting figures
-lat_min = float(monday_226[0][0])
-lat_max = float(lat_min)
-lon_min = float(monday_226[0][1])
-lon_max = float(lon_min)
-for pair in monday_226[1::1]:
-	lat = float(pair[0])
-	lon = float(pair[1])
-	if lat < lat_min:
-		lat_min = lat
-	elif lat > lat_max:
-		lat_max = lat
-	else:
-		pass
-	if lon < lon_min:
-		lon_min = lon
-	elif lon > lon_max:
-		lon_max = lon
-	else:
-		pass
-print("Lat range is {} to {}".format(lat_min, lat_max))
-print("Lon range in {} to {}".format(lon_min, lon_max))
+for i in range(0, 7, 1):
+	print("")
+	print(i)
+	print(len(all_positions[0]))
+	print(len(at_stop[0]))
+
+# coordinates = bus_stop_all_coordinates_at_stop("00010001.csv")
+# monday = coordinates[0]
+# monday_226 = monday['226']
+# # starting figures
+# lat_min = float(monday_226[0][0])
+# lat_max = float(lat_min)
+# lon_min = float(monday_226[0][1])
+# lon_max = float(lon_min)
+# for pair in monday_226[1::1]:
+# 	lat = float(pair[0])
+# 	lon = float(pair[1])
+# 	if lat < lat_min:
+# 		lat_min = lat
+# 	elif lat > lat_max:
+# 		lat_max = lat
+# 	else:
+# 		pass
+# 	if lon < lon_min:
+# 		lon_min = lon
+# 	elif lon > lon_max:
+# 		lon_max = lon
+# 	else:
+# 		pass
+# print("Lat range is {} to {}".format(lat_min, lat_max))
+# print("Lon range in {} to {}".format(lon_min, lon_max))
 
 
 # 	0	1	  2		3	4		5		6		7		8		9		10			11				12			13				14		15			16		17		  18		19		20		30		31		32				33			34				35			 	36				37			38					39				40			 41		42	
