723ba1d1289aaa8f53f7b4cda5c6ba52c47b9c24
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-07-19 00:01:22 +0100

    automated check of how many days of bus stop data are in each file

--- data/functions/JourneyPatternID/quality_bus_stops_all.py
+++ data/functions/JourneyPatternID/quality_bus_stops_all.py
@@ -3,6 +3,7 @@ import os
 import glob
 
 
+# making sure each file has seven lines
 def jpi_to_bus_all():
 	os.chdir("../../")
 	os.chdir("data/JourneyPatternID/bus_stops/all")
@@ -22,6 +23,23 @@ def check_lines_in_file(file_name, expected_lines):
 	jpi_to_bus_all()
 	lines = lines_in_file(file_name)
 	bus_all_to_jpi()
-	return lines == expected_lines
+	return [lines, lines == expected_lines]
 
-print(check_lines_in_file("00010001.csv", 7))
+def get_bus_stop_file_names():
+	file_names = []
+	jpi_to_bus_all()
+	for file in glob.glob("*.csv"):
+		file_names.append(file)
+	bus_all_to_jpi()
+	return file_names
+
+def check_lines():
+	with open("quality_bus_stops_all_seven_lines.txt", "w") as destination:
+		file_names = get_bus_stop_file_names()
+		for file_name in file_names:
+			result = check_lines_in_file(file_name, 7)
+			if result[1]:
+				destination.write("{}: {}".format(file_name, result[0]) + "\n")
+
+if __name__ == "__main__":
+	check_lines()
