2115dcb09ad4cdda301562bc128b27d0a9b9c268
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-07-19 16:00:17 +0100

    error correction

--- data/functions/JourneyPatternID/bus_stops/create_bus_stops.py
+++ data/functions/JourneyPatternID/bus_stops/create_bus_stops.py
@@ -13,7 +13,7 @@ def create_files():
 		the_journeys = generate_routes.routes(file_name)
 		all_stops = generate_bus_stop.unique_stops(the_journeys)
 		# create the object of interest
-		bus_route = bus_stops(all_stops, the_journeys)
+		bus_route = generate_bus_stop.bus_stops(all_stops, the_journeys)
 		# save the object of interest to file
 		general.bsf_to_bsd()
 		with open(file_name, "w") as destination:
