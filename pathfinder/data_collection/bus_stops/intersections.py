7333d73e962f3c55503682c082fceeaa737c0b84
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-07-20 14:40:50 +0100

    testing a function

---
+++ pathfinder/data_collection/bus_stops/intersections.py
@@ -0,0 +1,30 @@
+# imports
+import general
+import source_data
+
+
+def get_intersection_pairwise(primary_jpi, secondary_jpi, bus_stop_dict):
+	# data objects
+	intersection_list = []
+	primary_list = bus_stop_dict[primary_jpi]
+	secondary_list = bus_stop_dict[secondary_jpi]
+	# iteration to populate intersection_list
+	for stop_id in primary_list:
+		if stop_id in secondary_list:
+			intersection_list.append(stop_id)
+	return (secondary_jpi, intersection_list)
+
+def get_intersection_all_pairwise(primary_jpi, bus_stop_dict):
+	all_pairwise_intersections = {}
+	for jpi in bus_stop_dict:
+		if jpi == primary_jpi:
+			pass
+		else:
+			intersection_tuple = get_intersection_pairwise(primary_jpi, jpi, bus_stop_dict)
+			all_pairwise_intersections[jpi] = intersection_tuple[1]
+	return (primary_jpi, all_pairwise_intersections)
+
+bus_stop_dict = general.jpi_dictionary()
+primary_jpi = "00010001.csv"
+secondary_jpi = "00011001.csv"
+print(get_intersection_pairwise(primary_jpi, secondary_jpi, bus_stop_dict))
--- pathfinder/data_collection/bus_stops/source_data.py
+++ pathfinder/data_collection/bus_stops/source_data.py
@@ -1,24 +1,31 @@
 # imports
-import os
-import glob
 import general
 
 
 # JPI DICTIONARY
-# Output: a diciontary where the keys are JPIs and the values are the associated bus stops
+# Output: a diciontary where the keys are JPIs and the values are the associated bus stops in a list
+
+# returns a dictionary with key of jpi and values of False (to be replaced with a list of the bus stops)
 def skeleton_dictonary(bus_file_names_list):
 	return {jpi:False for jpi in bus_file_names_list}
 
+# creates a dicitonary with key of file name and values of associated bus stops
 def file_name_dictionary():
+	# a list with every bus stop file name in it
 	file_name_list = general.list_of_bus_files()
+	# a skeleton dictionary
 	bus_stop_dict = skeleton_dictonary(file_name_list)
+	# populate the skeleton dictionary with values: list of bust stops for the given jpi
 	for file_name in bus_stop_dict:
 		bus_stop_dict[file_name] = general.return_bus_stop_data(file_name)
+	# return
 	return bus_stop_dict
 
+# converts a csv file name into a name reflectin only the jpi
 def csv_file_name_to_name(file_name):
 	return file_name.strip().split(".")[0]
 
+# takes the return of file_name_dictionary and replaces full file name with just the jpi
 def jpi_dictionary():
 	bus_stop_dict = file_name_dictionary()
 	for file_name in bus_stop_dict:
