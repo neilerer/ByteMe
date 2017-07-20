7f8878c30b8250738925027bb3c4e315a4c6e162
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-07-20 15:06:22 +0100

    testing function

--- pathfinder/data_collection/bus_stops/intersections.py
+++ pathfinder/data_collection/bus_stops/intersections.py
@@ -3,6 +3,7 @@ import general
 import source_data
 
 
+# returns a tuple with the secondary_jpi and the stops on it that intersect with the primary_jpi
 def get_intersection_pairwise(primary_jpi, secondary_jpi, bus_stop_dict):
 	# data objects
 	intersection_list = []
@@ -12,8 +13,10 @@ def get_intersection_pairwise(primary_jpi, secondary_jpi, bus_stop_dict):
 	for stop_id in primary_list:
 		if stop_id in secondary_list:
 			intersection_list.append(stop_id)
+	# return
 	return (secondary_jpi, intersection_list)
 
+# generates a tuple of the primary_jpi and a dictionary of each connected jpi and the stops of intersection
 def get_intersection_all_pairwise(primary_jpi, bus_stop_dict):
 	all_pairwise_intersections = {}
 	for jpi in bus_stop_dict:
@@ -22,9 +25,11 @@ def get_intersection_all_pairwise(primary_jpi, bus_stop_dict):
 		else:
 			intersection_tuple = get_intersection_pairwise(primary_jpi, jpi, bus_stop_dict)
 			all_pairwise_intersections[jpi] = intersection_tuple[1]
-	return (primary_jpi, all_pairwise_intersections)
+	return all_pairwise_intersections
 
 bus_stop_dict = source_data.jpi_dictionary()
 primary_jpi = "00010001"
 secondary_jpi = "00011001"
-print(get_intersection_pairwise(primary_jpi, secondary_jpi, bus_stop_dict))
+# print(get_intersection_pairwise(primary_jpi, secondary_jpi, bus_stop_dict))
+for item in get_intersection_all_pairwise(primary_jpi, bus_stop_dict):
+	print(item)
