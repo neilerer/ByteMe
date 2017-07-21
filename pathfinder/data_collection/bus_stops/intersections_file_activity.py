94b67442db67bf7c41d1bb53c7ba3eca797899bd
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-07-21 15:12:22 +0100

    error correction

--- pathfinder/data_collection/bus_stops/intersections_file_activity.py
+++ pathfinder/data_collection/bus_stops/intersections_file_activity.py
@@ -5,11 +5,17 @@ import intersections
 
 
 def intersections_to_file():
+	# generate the dictionary
 	jpi_intersections = intersections.get_all_intersections()
+	# got the appropriate directory
 	general.dc_to_i()
+	# create the file
 	destination = open("jpi_intersections.p", "w")
+	# get the dictionary to disk
 	pickle.dump(jpi_intersections, destination)
+	# close the file
 	destination.close()
+	# return to the starting directory
 	general.i_to_dc()
 
 
