b243c25a45419748b621cf187267847c01607981
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-07-21 15:04:18 +0100

    error checking and clean up

---
+++ pathfinder/data_collection/bus_stops/intersections_file_activity.py
@@ -0,0 +1,16 @@
+# imports
+import pickle
+import general
+import intersections
+
+
+def intersections_to_file():
+	jpi_intersections = intersections.get_all_intersections()
+	general.dc_to_i()
+	with open("jpi_intersections.p", "w") as destination:
+		pickle.dump(jpi_intersections, destination)
+	general.i_to_dc()
+
+
+if __name__ == "__main__":
+	create_intersections()
