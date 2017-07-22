5cacf3730bd25524064ba0cfb7a19345cfe0e1e4
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-07-22 16:57:26 +0100

    testing

--- pathfinder/dijkstra/journey.py
+++ pathfinder/dijkstra/journey.py
@@ -1,6 +1,8 @@
 # imports
 import general
 
-for item in general.next_stop_from_file():
+bus_stop_dict = general.next_stop_from_file()
+for item in bus_stop_dict:
 	print(item)
+	print(bus_stop_dict[item])
 	print("")
