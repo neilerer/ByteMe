ee96c508bc47e6da99d2f2d66decb10176de2358
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-07-22 11:55:27 +0100

    prepped to merge with master

--- pathfinder/data_collection/bus_stops/next_stop_data.py
+++ pathfinder/data_collection/bus_stops/next_stop_data.py
@@ -36,4 +36,4 @@ def next_stop_from_file():
 
 if __name__ == "__main__":
 	status = (next_stop.next_for_all() == next_stop_from_file())
-	print("A new dictionary and that form file match: {}".format(status))
+	print("A new dictionary and that from file match: {}".format(status))
