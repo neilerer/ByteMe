a7f219a94292bd296f95c1ee2dfd57dc3d4c63cf
cojowy <16202597@ucdconnect.ie>
2017-07-29 13:20:24 +0100

    adding static data json files to data directory

---
+++ models/linear_model/training_linear_models.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+import random
+import numpy as np
+import os
+import pickle as pkl
+from itertools import permutations
