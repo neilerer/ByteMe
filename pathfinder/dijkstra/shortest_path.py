694e0c36ee543e528ca0c345275121fad6cd62ba
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-07-29 11:42:42 +0100

    settig up some server tests

--- pathfinder/dijkstra/shortest_path.py
+++ pathfinder/dijkstra/shortest_path.py
@@ -6,7 +6,7 @@ import data_conversion_routes_to_stops as dcrts
 
 
 
-def start_journey(start_stop_id, stop_dict, been_list, journey_id_list):
+def start_journey(start_stop_id, end_stop_id, stop_dict, been_list, journey_id_list):
 	"""
 	Purpose
 	- to generate the starting options for a journey on the bus system
@@ -31,12 +31,14 @@ def start_journey(start_stop_id, stop_dict, been_list, journey_id_list):
 
 	Output
 	- list
-	-- list[0] boolean indicating if we've found the shortest path (in this function it will always be False)
+	-- list[0] boolean indicating if we've found the shortest path
 	-- list[1] a dictionary
 	--- key journey_id
 	--- value [total travel time, [(starting stop id, next stop id, travel time, route id)
 
 	"""
+	if start_stop_id == end_stop_id:
+		return [True, {1: [0.00, [(start_stop_id, end_stop_id, 0.00, "n/a")]]}]
 	# temp_dict
 	temp_dict = dict()
 	# instantiate journey_id
@@ -107,7 +109,7 @@ def find_shortest_path(start_stop_id, end_stop_id, stop_dict):
 	this_journey_id_list = []
 	this_journies_dict = dict()
 
-	result = start_journey(start_stop_id, stop_dict, this_been_list, this_journey_id_list)
+	result = start_journey(start_stop_id, end_stop_id, stop_dict, this_been_list, this_journey_id_list)
 	this_journies_dict = result[1]
 
 	while this_found_shortest_path is False:
@@ -124,124 +126,7 @@ def find_shortest_path(start_stop_id, end_stop_id, stop_dict):
 if __name__ == "__main__":
 	# data objects
 	stop_dict = dcrts.get_bus_stop_data()
-	# print(find_shortest_path(72, 82, stop_dict))
-	print(find_shortest_path(32, 73, stop_dict))
-	print(find_shortest_path(72, 20, stop_dict))
-
-
-
-
-
-
-
-
-	# been_list = []
-	# journey_id = 0
-	# journey_id_list = []
-	# journies_dict = dict()
-	
-	# shortest_path = None
-	# found_shortest_path = False
-
-	# for stop in stop_dict:
-	# 	print(stop)
-	# 	print(stop_dict[stop])
-	# 	print("")
-
-	# start_journey(78, stop_dict, been_list, journey_id_list, journies_dict)
-	# print("starting journey dict")
-	# print(journies_dict)
-	# print("")
-
-	# continue_journey(79, stop_dict, been_list, journey_id_list, journies_dict, shortest_path, found_shortest_path)
-	# print("journeys at depth 1")
-	# print(journies_dict)
-	# print("")
-
-	# print(shortest_path)
-
-
-
-# def continue_journey(end_stop_id, stop_dict, been_list, journey_id_list, journies_dict, shortest_path, found_shortest_path):
-# 	# instatiate journey_id
-# 	journey_id = journey_id_list[-1] + 1
-# 	# sort journies so we know which is the shortest
-# 	starting_dict = merge_sort.merge_sort_journies_dict(journies_dict)
-# 	# get the starting details
-# 	starting_dict_length = len(starting_dict)
-# 	for i in range(0, starting_dict_length, 1):
-# 		starting_key = next(iter(starting_dict))
-# 		starting_details = starting_dict.pop(next(iter(starting_dict)))
-# 		journey_time = starting_details[0]
-# 		journey_details = starting_details[1]
-# 		start_stop_id = journey_details[-1][0]
-# 		next_stop_id = journey_details[-1][1]
-# 		# # delete the journey that does should not be there any longer
-# 		# del journies_dict[starting_key]
-# 		# check if we are at the destination
-# 		if next_stop_id == end_stop_id:
-# 			shortest_path = starting_details
-# 			found_shortest_path = True
-# 		# elif next_stop_id in been_list:
-# 			# pass
-# 		else:
-# 			been_list.append(next_stop_id)
-# 			list_of_next_stop_details = stop_dict[next_stop_id]
-# 			# populate journies_dict
-# 			for stop_detail in list_of_next_stop_details:
-# 				current_stop_id = stop_detail[1]
-# 				next_time = stop_detail[2]
-# 				# check if we've already been there
-# 				if current_stop_id not in been_list:
-# 					# record the journey_id
-# 					journey_id_list.append(journey_id)
-# 					# create the journey details
-# 					journey_details.append(stop_detail)
-# 					journies_dict[journey_id] = [round(journey_time + next_time, 2), journey_details]
-# 					# increment the journey_id
-# 					journey_id = journey_id_list[-1] + 1
-
-
-
-# def continue_journey(end_stop_id, stop_dict, been_list, journey_id_list, journies_dict, shortest_path, found_shortest_path):
-# 	# instatiate journey_id
-# 	journey_id = journey_id_list[-1] + 1
-# 	# sort journies so we know which is the shortest
-# 	starting_dict = merge_sort.merge_sort_journies_dict(journies_dict)
-# 	temp_dict = dict()
-# 	# 
-# 	for jid in starting_dict:
-# 		starting_details = starting_dict[jid]
-# 		journey_time = starting_details[0]
-# 		journey_details = starting_details[1]
-# 		start_stop_id = journey_details[-1][0]
-# 		next_stop_id = journey_details[-1][1]
-# 		# 
-# 		if next_stop_id == end_stop_id:
-# 			shortest_path = starting_details
-# 			found_shortest_path = True
-# 			temp_dict = {}
-# 			temp_dict[journey_id] = [round(journey_time + next_time, 2), journey_details]
-# 			return [temp_dict, True]
-# 		#
-# 		elif next_stop_id in been_list:
-# 			pass
-# 		#
-# 		else:
-# 			been_list.append(next_stop_id)
-# 			list_of_next_stop_details = stop_dict[next_stop_id]
-# 			# 
-# 			for stop_detail in list_of_next_stop_details:
-# 				current_stop_id = stop_detail[1]
-# 				next_time = stop_detail[2]
-# 				# check if we've already been there
-# 				if current_stop_id not in been_list:
-# 					# record the journey_id
-# 					journey_id_list.append(journey_id)
-# 					# create the journey details
-# 					journey_details.append(stop_detail)
-# 					# create an entry
-# 					temp_dict[journey_id] = [round(journey_time + next_time, 2), journey_details]
-# 					# increment the journey_id
-# 					journey_id = journey_id_list[-1] + 1
-# 	return [temp_dict, False]
+	for stop in stop_dict:
+		print("")
+		print(stop)
+		print(find_shortest_path(stop, 76, stop_dict))
