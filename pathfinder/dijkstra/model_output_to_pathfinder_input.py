034ad6e7a87c6ce7c97a745b4df1bccb74b56def
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-07-29 17:58:24 +0100

    model output to pathfinder input is ready; will test on full data tomorrow

--- pathfinder/dijkstra/model_output_to_pathfinder_input.py
+++ pathfinder/dijkstra/model_output_to_pathfinder_input.py
@@ -25,7 +25,7 @@ def dict_display(file_name, jpi):
 		print("")
 
 def route(jpi_dict, jpi, weekday_input, hour_input):
-	data = jpi_dict[jpi]	
+	data = jpi_dict[jpi]
 
 	output_dict = dict()
 
@@ -38,7 +38,7 @@ def route(jpi_dict, jpi, weekday_input, hour_input):
 		ctt = data[key]
 
 		if weekday == weekday_input and hour == hour_input:
-			output_dict[stop_sequence] = (stop_id, ctt)
+			output_dict[stop_sequence] = (stop_id, ctt, jpi)
 
 	output_dict = merge_sort.merge_sort_route(output_dict)
 
@@ -47,25 +47,30 @@ def route(jpi_dict, jpi, weekday_input, hour_input):
 def route_list(output_dict):
 	stop_id_list = []
 	ctt_list = []
+	jpi_list = []
 	for stop_sequence in output_dict:
 		value = output_dict[stop_sequence]
 		stop_id = value[0]
 		ctt = value[1]
+		jpi = value[2]
 		stop_id_list.append(stop_id)
 		ctt_list.append(ctt)
-	return [stop_id_list, ctt_list]
+		jpi_list.append(jpi)
+	return [stop_id_list, ctt_list, jpi_list]
 
 def pathfinder_input(route_list_output):
 	pathfinder_dict = dict()
 	stop_id_list = route_list_output[0]
 	ctt_list = route_list_output[1]
+	jpi_list = route_list_output[2]
 	index = 1
 	for stop_id in stop_id_list[1::1]:
-		start_stop_id = stop_id_list[index - 1]
-		next_stop_id = stop_id_list[index]
+		start_stop_id = int(stop_id_list[index - 1])
+		next_stop_id = int(stop_id_list[index])
 		time = ctt_list[index] - ctt_list[index - 1]
-		triple = (start_stop_id, next_stop_id, time)
-		pathfinder_dict[start_stop_id] = triple
+		route = jpi_list[index - 1][0:5]
+		quadruple = (start_stop_id, next_stop_id, time, route)
+		pathfinder_dict[start_stop_id] = [quadruple]
 		index += 1
 	return pathfinder_dict
 
