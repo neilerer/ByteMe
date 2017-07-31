92d83142f9dd16fb8240ef0451649665245f3e56
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-07-31 11:22:07 +0100

    preventing looping between two stops

--- pathfinder/a_star/shortest_path.py
+++ pathfinder/a_star/shortest_path.py
@@ -75,7 +75,6 @@ def continue_journey(journey_id_list, journies_dict, been_list, end_stop_id, sto
 	intermediate_dict = merge_sort.merge_sort_journies_dict(journies_dict)
 	# sort journies so that a journey on the route gets precedence
 	starting_dict = the_heuristic.target_route_goes_first(intermediate_dict, target_routes)
-	print(starting_dict)
 	# temp_dict that is the return object
 	temp_dict = dict()
 	# delete list
@@ -88,6 +87,7 @@ def continue_journey(journey_id_list, journies_dict, been_list, end_stop_id, sto
 		journey_details = starting_details[1]
 		start_stop_id = journey_details[-1][0]
 		next_stop_id = journey_details[-1][1]
+		current_route = journey_details[-1][3]
 		# skip if the next_stop has been visted before
 		if next_stop_id in been_list:
 			pass
@@ -104,8 +104,9 @@ def continue_journey(journey_id_list, journies_dict, been_list, end_stop_id, sto
 			for stop_detail in next_dict:
 				next_dict_stop_id = stop_detail[1]
 				next_dict_time = stop_detail[2]
+				possible_route = stop_detail[3]
 				# if the stop has not been visited
-				if next_dict_stop_id not in been_list:
+				if next_dict_stop_id not in been_list and possible_route != (current_route *-1): # need to change the criteria for the reverse route for actual data
 					delete_list.add(jid) # we've extended this journey, so after temp_dict is full, delete this entry
 					journey_id = journey_id_list[-1] + 1
 					journey_id_list.append(journey_id)
