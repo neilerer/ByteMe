b8228187a8f0d8f672b33789bcdc16660ef1f31d
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-07-31 11:44:19 +0100

    changed the heuristic return; might solve the issue

--- pathfinder/a_star/the_heuristic.py
+++ pathfinder/a_star/the_heuristic.py
@@ -7,20 +7,25 @@ def target_route_goes_first(journies_dict, target_routes):
 	# data
 	return_dict = dict()
 	heuristic_list = list()
+	secondary_list = list()
 	# iterate over each journey in journies_dict
-	for jpi in journies_dict:
+	for jid in journies_dict:
 		# unpack the data
-		value = journies_dict[jpi]
+		value = journies_dict[jid]
 		details = value[1]
 		quadruple = details[-1]
 		route = quadruple[3]
 		# if the journey is on the target route, put it at the front of the line
 		if route in target_routes:
-			heuristic_list.append(jpi)
+			heuristic_list.append(jid)
+		else:
+			secondary_list.append(jid)
 	# return
 	if len(heuristic_list) > 0:
-		for target_jpi in heuristic_list:
-			return_dict[target_jpi] = journies_dict[target_jpi]
+		for target_jid in heuristic_list:
+			return_dict[target_jid] = journies_dict[target_jid]
+		for target_jid in secondary_list:
+			return_dict[target_jid] = journies_dict[target_jid]
 		return return_dict
 	else:
 		return journies_dict
@@ -30,21 +35,21 @@ def target_route_goes_first(journies_dict, target_routes):
 	# second_list = list()
 	# return_dict = dict()
 	# # iterate over each journey in journies_dict
-	# for jpi in journies_dict:
+	# for jid in journies_dict:
 	# 	# unpack the data
-	# 	value = journies_dict[jpi]
+	# 	value = journies_dict[jid]
 	# 	details = value[1]
 	# 	quadruple = details[-1]
 	# 	route = quadruple[3]
 	# 	# if the journey is on the target route, put it at the front of the line
 	# 	if route in target_routes:
-	# 		first_list.append(jpi)
+	# 		first_list.append(jid)
 	# 	else:
-	# 		second_list.append(jpi)
+	# 		second_list.append(jid)
 	# 	# populate return_dict
 	# 	if len(first_list) > 0:
-	# 		for jpi in first_list:
-	# 			return_dict[jpi] = journies_dict[jpi]
+	# 		for jid in first_list:
+	# 			return_dict[jid] = journies_dict[jid]
 	# 	else:
 	# 		for jp in se
 	# # return
