be64a4441ec8f738cf2b34ad1d63e6ed623c2b75
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-08-05 00:21:01 +0100

    testing

--- pathfinder/mod_a_star/data_linked_list/linked_list.py
+++ pathfinder/mod_a_star/data_linked_list/linked_list.py
@@ -32,7 +32,7 @@ def model_data_review_all_contents(model_dict):
 				print(time_unit_data[stop])
 				# stop : [(start, next, time, route), . . . ]
 
-def model_data_review_lengths(model_dict):
+def model_data_review_number_of_stops(model_dict):
 	# open the destination file
 	destination = open("model_data_lengths.txt", "w")
 	# iterate over weekdays
@@ -51,8 +51,29 @@ def model_data_review_lengths(model_dict):
 		destination.write("\n\n\n\n")
 	destination.close()
 
+def model_data_review_stop_lengths(model_dict):
+	# open the destination file
+	destination = open("model_data_stop_lengths.txt", "w")
+	# iterate over weekdays
+	for weekday in model_dict:
+		# record the weekday
+		destination.write("Day of the Week: {}\n\n".format(weekday))
+		# go into the weekday data
+		weekday_data = model_dict[weekday]
+		# iterate over time_units
+		for time_unit in weekday_data:
+			# record the time unit
+			destination.write("Time Unit: {}\n".format(time_unit))
+			# got into the time_unit data
+			time_unit_data = weekday_data[time_unit]
+			for stop in time_unit_data:
+				stop_data = time_unit_data[stop]
+				destination.write("{} has {} links\n\n".format(stop, len(stop_data)))
+		destination.write("\n\n\n\n")
+	destination.close()
+
 model_dict = get_model_data()
-model_data_review_lengths(model_dict)
+model_data_review_stop_lengths(model_dict)
 
 
 def dict_to_list(model_data):
