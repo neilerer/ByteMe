edd4f8edd6d19c1c4ddb124fe3d92c25d25a3bae
cojowy <16202597@ucdconnect.ie>
2017-08-04 12:04:00 +0100

    bus stop names added with function for getting them

--- data/functions/stop_coordinates/stop_names.py
+++ data/functions/stop_coordinates/stop_names.py
@@ -1,29 +1,44 @@
 import geopy
 import json
+from time import sleep
+
 geolocator = geopy.Nominatim()
 
-with open("../../static_data/stop_coordinates.json") as file:
+with open("stop_coordinates.json") as file:
 	coords_dict=json.load(file)
 
-with open("../../static_data/stop_coordinates_with_names.json") as namefile:
-	names_dict=json.load(namefile)
+with open("stop_coordinates_with_names.json") as namefile:
+	result_dict=json.load(namefile)
 
-for key in names_dict:
-	print(key,names_dict[key])
-	
-if False:
-	result_dict={}
+if False: # code was used to query geopy for the name of each bus stop location
+	new_stops=[]
 	for stop in coords_dict:
-		if stop not in names_dict:
+		if stop not in result_dict:
+			new_stops.append(stop)
+
+
+	print("BEFORE:",len(result_dict))
+
+	for stop in new_stops:
+		sleep(1)
+		if stop not in result_dict:
 			try:
 				location = geolocator.reverse(str(coords_dict[stop][0])+","+str(coords_dict[stop][1]))		
 				result_dict[stop]=[coords_dict[stop][0],coords_dict[stop][1],location.address]
+				print("added new stop")
 			except:
-				print("except block")
-				continue
-			
-	with open("../../static_data/stop_coordinates_with_names.json","w",newline="") as result_file:
-				json.dump(result_dict,result_file)
+				with open("stop_coordinates_with_names.json","w",newline="") as result_file:
+					json.dump(result_dict,result_file)
+				break
+				
+	with open("stop_coordinates_with_names.json","w",newline="") as result_file:
+					json.dump(result_dict,result_file)
 
+	print("AFTER:",len(result_dict))
 
+for stop in result_dict: # chopping off the end of the result
+	split_list=result_dict[stop][2].split(",")
+	result_dict[stop][2]=",".join(split_list[:4])
 
+with open("stop_coordinates_with_names.json","w",newline="") as result_file:
+					json.dump(result_dict,result_file)
