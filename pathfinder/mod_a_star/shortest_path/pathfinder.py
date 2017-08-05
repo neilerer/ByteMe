c91f2c31cf582ffb907aff86a8566774eb60bb5b
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-08-05 17:37:08 +0100

    testing review function

--- pathfinder/mod_a_star/shortest_path/pathfinder.py
+++ pathfinder/mod_a_star/shortest_path/pathfinder.py
@@ -5,7 +5,8 @@ import pickle
 
 
 def get_time_unit_path_dict(weekday, time_unit):
-	file_name = str(weekday) + "_" + str(time_unit) + ".p"
+	key = str(weekday) + "_" + str(time_unit)
+	file_name = key + ".p"
 	os.chdir("../")
 	os.chdir("possible_paths")
 	f = open(file_name, "rb")
@@ -13,11 +14,28 @@ def get_time_unit_path_dict(weekday, time_unit):
 	f.close()
 	os.chdir("../")
 	os.chdir("shortest_path")
-	return time_unit_path_dict
+	return [key, time_unit_path_dict]
 
 
+# {'0_7': {('00590', '00590'): {0: ['00590']}, ('00590', '01111'): {0: ['00590', '01111']}, ('00590', '045A0'): {1: ['00590', '045A0']}, ('00590', '00070'): {2: ['00590', '00070']}, ('00590', '00080'): {3: ['00590', '00080']}, ('00590', '00081'): {4: ['00590', '00081']}
 
-time_unit_path_dict = get_time_unit_path_dict(0, 7)
-for time_unit_dict in time_unit_path_dict:
-	for item in time_unit_dict:
-		print(item)
+def time_unit_path_dict_review(weekday, time_unit):
+	source = get_time_unit_path_dict(weekday, time_unit)
+	key = source[0]
+	time_unit_path_dict = source[1]
+
+	time_unit_dict = time_unit_path_dict[key]
+
+	for path_id in time_unit_dict:
+		path = next(iter(time_unit_dict[path_id]))
+		print(path_id)
+		print(path)
+		print("")
+
+time_unit_path_dict_review(0, 7)
+
+
+# time_unit_path_dict = get_time_unit_path_dict(0, 7)
+# for time_unit_dict in time_unit_path_dict:
+# 	for item in time_unit_dict:
+# 		print(item)
