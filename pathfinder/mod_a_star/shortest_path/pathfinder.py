d1c31f37d0b6a05f5b9ba0fc95536b501dc3aa08
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-08-05 17:24:27 +0100

    prepping for final stage of pathfinder

---
+++ pathfinder/mod_a_star/shortest_path/pathfinder.py
@@ -0,0 +1,20 @@
+# imports
+import os
+import pickle
+
+
+
+def get_time_unit_path_dict(weekday, time_unit):
+	file_name = str(weekday) + "_" + str(time_unit) + ".p"
+	os.chdir("../")
+	os.chdir("possible_paths")
+	f = open(file_name, "rb")
+	time_unit_path_dict = pickle.load(f)
+	f.close()
+	os.chdir("../")
+	os.chdir("shortest_path")
+	return time_unit_path_dict
+
+
+
+print(get_time_unit_path_dict(0, 7))
