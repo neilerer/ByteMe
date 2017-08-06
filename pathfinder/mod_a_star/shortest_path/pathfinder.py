bb31bdded69d636065ce980fa14860431e688470
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-08-06 10:29:17 +0100

    testing

--- pathfinder/mod_a_star/shortest_path/pathfinder.py
+++ pathfinder/mod_a_star/shortest_path/pathfinder.py
@@ -4,23 +4,41 @@ import glob
 import pickle
 
 
-
-def get_time_unit_path_dict(weekday, time_unit):
-	key = str(weekday) + "_" + str(time_unit)
-	file_name = key + ".p"
+def sp_to_pp():
 	os.chdir("../")
 	os.chdir("possible_paths")
+
+def pp_to_sp():
+	os.chdir("../")
+	os.chdir("shortest_path")
+
+
+
+def get_time_unit_path_dict_by_units(weekday, time_unit):
+	key = str(weekday) + "_" + str(time_unit)
+	file_name = key + ".p"
+	sp_to_pp()
 	f = open(file_name, "rb")
 	time_unit_path_dict = pickle.load(f)
 	f.close()
-	os.chdir("../")
-	os.chdir("shortest_path")
+	pp_to_sp()
 	return [key, time_unit_path_dict]
 
 
-# {'0_7': {('00590', '00590'): {0: ['00590']}, ('00590', '01111'): {0: ['00590', '01111']}, ('00590', '045A0'): {1: ['00590', '045A0']}, ('00590', '00070'): {2: ['00590', '00070']}, ('00590', '00080'): {3: ['00590', '00080']}, ('00590', '00081'): {4: ['00590', '00081']}
+
+def get_time_unit_path_dict_by_file_name(file_name):
+	sp_to_pp()
+	f = open(file_name, "rb")
+	time_unit_path_dict = pickle.load(f)
+	f.close()
+	pp_to_sp()
+	key = file_name.strip().split(".")[0]
+	return [key, time_unit_path_dict]
+
+
 
 def time_unit_path_dict_review(weekday, time_unit):
+	# {'0_7': {('00590', '00590'): {0: ['00590']}, ('00590', '01111'): {0: ['00590', '01111']}, ('00590', '045A0'): {1: ['00590', '045A0']}, ('00590', '00070'): {2: ['00590', '00070']}, ('00590', '00080'): {3: ['00590', '00080']}, ('00590', '00081'): {4: ['00590', '00081']}
 	source = get_time_unit_path_dict(weekday, time_unit)
 	key = source[0]
 	time_unit_path_dict = source[1]
@@ -34,9 +52,9 @@ def time_unit_path_dict_review(weekday, time_unit):
 		print("")
 
 
+
 def get_time_unit_path_dict_file_names():
-	os.chdir("../")
-	os.chdir("possible_paths")
+	sp_to_pp()
 	file_name_list = list()
 	for file_name in glob.glob("*.p"):
 		try:
@@ -44,8 +62,22 @@ def get_time_unit_path_dict_file_names():
 				file_name_list.append(file_name)
 		except:
 			pass
-	os.chdir("../")
-	os.chdir("shortest_path")
+	pp_to_sp()
 	return file_name_list
 
-print(get_time_unit_path_dict_file_names())
+
+
+def combine_time_unit_path_dicts():
+	time_unit_path_dicts = dict()
+	file_name_list = get_time_unit_path_dict_file_names()
+	for file_name in file_name_list:
+		new_items = get_time_unit_path_dict_by_file_name(file_name)
+		key = new_items[0]
+		time_unit_path_dict = new_items[1]
+		time_unit_path_dicts[key] = time_unit_path_dict
+	return time_unit_path_dicts
+
+
+
+if __name__ == "__main__":
+	combine_time_unit_path_dicts()
