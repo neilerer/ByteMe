5e85894960cac2061d9e265e18968036c447564b
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-08-06 11:43:21 +0100

    changing test messages

--- pathfinder/mod_a_star/shortest_path/shortest_path.py
+++ pathfinder/mod_a_star/shortest_path/shortest_path.py
@@ -17,5 +17,9 @@ if __name__ == "__main__":
 	journey_details = user_input.user_input(start_stop_id, end_stop_id, weekday, time_unit, model_dict)
 	start_details = journey_details[0]
 	end_details = journey_details[1]
+	print("Start Details")
 	print(start_details)
+	print("")
+	print("End Details")
 	print(end_details)
+	print("")
