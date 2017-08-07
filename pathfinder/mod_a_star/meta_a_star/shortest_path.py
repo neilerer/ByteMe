590a2bc699a9a7fa9f494a36423bc6c5503a8c53
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-08-07 11:48:55 +0100

    updatd shortest path; testing

--- pathfinder/mod_a_star/meta_a_star/shortest_path.py
+++ pathfinder/mod_a_star/meta_a_star/shortest_path.py
@@ -1,6 +1,6 @@
 # imports
 import copy
-
+import merge_sort
 
 
 def get_time_unit_connections_dict(connections_dict, weekday, time_unit):
@@ -41,7 +41,7 @@ def start_journey(start_route_id, end_route_id, time_unit_connections_dict, jour
 	if start_route_id == end_route_id:
 		return [True, {0: [None, [start_route_id]]}]
 	# temp_dict
-	temp_dict = dict()
+	ending_dict = dict()
 	# instantiate journey_id
 	try:
 		journey_id = journey_id_list[-1] + 1
@@ -58,48 +58,55 @@ def start_journey(start_route_id, end_route_id, time_unit_connections_dict, jour
 			# record the journey_id
 			journey_id_list.append(journey_id)
 			# create the journey details
-			temp_dict[journey_id] = [been_set, [start_route_id, route_id]]
+			ending_dict[journey_id] = [been_set, [start_route_id, route_id]]
 			# increment the journey_id
 			journey_id += 1
-	return [False, temp_dict]
+	return [False, ending_dict]
 
 def continue_journey(journey_id_list, journies_dict, end_route_id, time_unit_connections_dict):
 	# sort journies so we know which is the shortest
-	starting_dict = journies_dict
-	# temp_dict that is the return object
-	ending_dict = dict()
-	# iterate over items in starting_dict
-	for journey_id in starting_dict:
-		# details
-		path_details = starting_dict[journey_id]
-		been_set = path_details[0]
-		next_route_id = path_details[1][-1]
-		# if the next_stop is our destination, then we have our route
-		if next_route_id == end_route_id:
-			ending_dict = dict()
-			ending_dict[journey_id] = path_details
-			return [True, ending_dict]
-		# otherwise, we create the next set of possible paths
-		else:
-			# we get the information about the next possible routes
-			next_list = time_unit_connections_dict[next_route_id]
-			# iterate over possible next stops
-			for next_route_id in next_list:
-				if next_route_id not in been_set:
-					# add to the been_set
-					been_set.add(next_route_id)
-					# create a new journey id
-					journey_id = journey_id_list[-1] + 1
-					journey_id_list.append(journey_id)
-					# populate ending_dict
-					new_details = copy.deepcopy(path_details)
-					new_details.append(next_route_id)
-					ending_dict[journey_id] = new_details
-	# return
-	if not ending_dict:
-		return [True, {0: [None, None]}]
+	continuing_dict = journies_dict
+	# sort continuing_dict by length of route
+	continuing_dict = merge_sort.merge_sort_path_dict(continuing_dict)
+	# continue the shortest path
+	current_path_details = merge_sort.remove_first_entry_of_dict(continuing_dict)
+	journey_id = current_path_details[0]
+	been_set = current_path_details[1][0]
+	current_path = current_path_details[1][1]
+	current_route_id = current_path[-1]
+	# termination condition
+	if current_route_id == end_route_id:
+		return [True, {journey_id: current_path_details}]
 	else:
-		return [False, ending_dict]
+		# get the information about the next possible routes
+		next_list = time_unit_connections_dict[current_route_id]
+		# iterate over possible next routes
+		for next_route_id in next_list:
+			# make sure we haven't been on it before
+			if next_route_id not in been_set:
+				# add to the been set
+				been_set.add(next_route_id)
+				# create a new journey id
+				journey_id = journey_id_list[-1] + 1
+				journey_id_list.append(journey_id)
+				# populate ending_dict
+				new_path = list()
+				for item in current_path:
+					new_path.append(item)
+				new_path.append(next_route_id)
+				continuing_dict[journey_id] = [been_set, new_path]
+	# return
+	if not continuing_dict:
+		new_path = list()
+			for item in current_path:
+				new_path.append(item)
+			journey_path.append("There is no path from {} to {}".format(start_route_id, end_route_id))
+			continuing_dict[journey_id] = [been_set, new_path]
+			[True, continuing_dict]
+	else:
+		return [False, continuing_dict]
+
+
 
 # def continue_journey(journey_id_list, journies_dict, been_list, end_route_id, time_unit_connections_dict):
 # 	# sort journies so we know which is the shortest
