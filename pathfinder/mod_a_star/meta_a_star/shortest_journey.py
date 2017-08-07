f3910c18c854e98f6d450934d2600f9270902336
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-08-07 07:55:30 +0100

    testing

--- pathfinder/mod_a_star/meta_a_star/shortest_journey.py
+++ pathfinder/mod_a_star/meta_a_star/shortest_journey.py
@@ -115,6 +115,9 @@ def find_shortest_journey(weekday, time_unit, start_stop_id, end_stop_id, path, 
 	result = continue_journey(weekday, time_unit, start_stop_id, end_stop_id, path, model_dict, journey_id_list, journies_dict)
 	found_shortest_path = result[0]
 	journies_dict = result[1]
+	result = continue_journey(weekday, time_unit, start_stop_id, end_stop_id, path, model_dict, journey_id_list, journies_dict)
+	found_shortest_path = result[0]
+	journies_dict = result[1]
 	# while found_shortest_path is False:
 	# 	result = continue_journey(weekday, time_unit, start_stop_id, end_stop_id, path, model_dict, journey_id_list, journies_dict)
 	# 	found_shortest_path = result[0]
