a18edbd80bace2896b7a54a4758880161a100dd3
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-08-07 06:17:06 +0100

    nope; something else is amiss

--- pathfinder/mod_a_star/meta_a_star/shortest_journey.py
+++ pathfinder/mod_a_star/meta_a_star/shortest_journey.py
@@ -79,7 +79,7 @@ def continue_journey(weekday, time_unit, start_stop_id, end_stop_id, path, model
 			journey_path = current_journey_path.append(quadruple)
 			continuing_dict[journey_id] = [current_journey_time + next_stop_journey_time, been_set, journey_path]
 	# return
-	if continuing_dict:
+	if not continuing_dict:
 		return [True, {0: None}]
 	else:
 		return [False, continuing_dict]
