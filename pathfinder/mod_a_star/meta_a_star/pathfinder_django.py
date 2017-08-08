5ada3701dc686eb7fa7ad2eb26c87d0f5fe5ae7b
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-08-08 11:18:40 +0100

    testing

--- pathfinder/mod_a_star/meta_a_star/pathfinder_django.py
+++ pathfinder/mod_a_star/meta_a_star/pathfinder_django.py
@@ -90,7 +90,7 @@ def run_the_pathfinder():
 	wd_list = weekday_list()
 	tu_list = time_unit_list(wd_list)
 	si_dict = stop_id_dict()
-	print(si_dict)
+	print("all OK")
 
 	# model_dict = data.get_model_data()
 	# connections_dict = data.get_connections_dict()
