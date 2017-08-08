43f9d6b19d958e8cec4ed7ad5550ffc0337dbbd3
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-08-08 20:13:48 +0100

    testing

--- pathfinder/django_pathfinder/pathfinder/pathfinder.py
+++ pathfinder/django_pathfinder/pathfinder/pathfinder.py
@@ -49,8 +49,8 @@ def pathfinder(weekday, time_unit, start_stop_id, end_stop_id, model_dict, conne
 
 if __name__ == "__main__":
 	# testing user input
-	start_stop_id =  400#765# 
-	end_stop_id = 807#462#  
+	start_stop_id =  4952#765# 
+	end_stop_id = 1279#462#  
 	weekday = 0
 	time_unit = 10
 	journey_details = user_input.user_input(start_stop_id, end_stop_id, weekday, time_unit, model_dict)
