f108a1a1b11876a789bcc2f0513e7ffda5c84346
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-08-09 15:36:37 +0100

    testing

---
+++ models/running_average/generate_stop_format_data.py
@@ -0,0 +1,21 @@
+# imports
+import change_directories
+import get_file_names
+import source_data
+
+
+
+def get_jpi_names():
+	change_directories.to_jpi()
+	print(get_file_names.all_csv())
+	change_directories.from_jpi()
+
+
+
+jpi_names = get_jpi_names()
+for jpi in jpi_names:
+	change_directories.to_jpi()
+	jpi_data = source_data.open_jpi(jpi)
+	jpi_data.close()
+	change_directories.from_jpi()
+	print(jpi)
--- models/running_average/testing.py
+++
@@ -1,9 +0,0 @@
-# imports
-import change_directories
-import get_file_names
-
-
-
-change_directories.to_jpi()
-print(get_file_names.all_csv())
-change_directories.from_jpi()
