143629b641c3a316f46b921de5ca60cf031b1a63
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-08-09 15:50:19 +0100

    testing

--- models/running_average/generate_stop_format_data.py
+++ models/running_average/generate_stop_format_data.py
@@ -13,10 +13,19 @@ def get_jpi_names():
 
 
 
-jpi_names = get_jpi_names()
-for jpi in jpi_names:
+def get_headers():
+	# data objects
+	headers_list = list()
+	jpi_names = get_jpi_names()
 	change_directories.to_jpi()
-	jpi_data = source_data.open_jpi(jpi)
-	jpi_data.close()
+	for jpi in jpi_names:
+		jpi_data = source_data.open_jpi(jpi)
+		header = jpi_data.readline().strip().split(",")
+		headers_list.append(header)
+		jpi_data.close()
 	change_directories.from_jpi()
-	print(jpi)
+	return headers_list
+
+headers_list = get_headers()
+for header in headers_list:
+	print(header)
