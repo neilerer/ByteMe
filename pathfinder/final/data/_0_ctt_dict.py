7d61d761eada70c374740ef653b82ee591e91bc9
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-08-13 13:33:49 +0100

    breaking up generation of new data into parts for issues related to memory

--- pathfinder/final/data/_0_ctt_dict.py
+++ pathfinder/final/data/_0_ctt_dict.py
@@ -16,25 +16,35 @@ def unpack_observation(json_data, jpi, key):
 	# return (weekday, time_unit, route, stop_sequence, stop_id, ctt)
 	return (int(key_list[2]), int(key_list[3]), jpi[0:5], int(key_list[0]), int(key_list[1]), float(json_data[jpi][key]))
 
-def generate_ctt_dict(file_name):
-	# shell
+
+
+def generate_ctt_dict(json_data):
+	# high level structure
 	ctt_dict = dict()
 	for i in range(0, 7, 1):
 		ctt_dict[i] = dict()
 		for j in range(0, 24, 1):
 			ctt_dict[i][j] = dict()
-	# source data
-	json_data = json_to_dict(file_name)
-	# populate ctt_dict
+	# detailed structure
 	for jpi in json_data:
 		for key in json_data[jpi]:
-			for key in json_data[jpi]:
-				data = unpack_observation(json_data, jpi, key)
-				destination = ctt_dict[data[0]][data[1]]
-				if data[2] in destination:
-					destination[data[2]].append((data[3], data[4], data[5]))
-				else:
-					destination[data[2]] = [(data[3], data[4], data[5])]
+			data = unpack_observation(json_data, jpi, key)
+			destination = ctt_dict[data[0]][data[1]]
+			if data[2] in destination:
+				pass
+			else:
+				destination[data[2]] = list()
+	return ctt_dict
+
+def populate_ctt_dict_for_given_jpi(json_data, ctt_dict, jpi):
+	# populate ctt_dict
+	for key in json_data[jpi]:
+		for key in json_data[jpi]:
+			data = unpack_observation(json_data, jpi, key)
+			destination = ctt_dict[data[0]][data[1]]
+			destination[data[2]].append((data[3], data[4], data[5]))
+
+def sort_ctt_dict(ctt_dict):
 	# sort the route-level data
 	for weekday in ctt_dict:
 		for time_unit in ctt_dict[weekday]:
@@ -47,7 +57,12 @@ def generate_ctt_dict(file_name):
 
 if __name__ == "__main__":
 	# data
-	ctt_dict = generate_ctt_dict("data.json")
+	json_data = json_to_dict("data.json")
+	# generate ctt_dict
+	ctt_dict = generate_ctt_dict(json_data)
+	ctt_dict = populate_ctt_dict_for_given_jpi(json_data, ctt_dict, "077A1001")
+	ctt_dict = sort_ctt_dict(ctt_dict)
+	# get it to file
 	destination = open("ctt_dict.p", "wb")
 	# dump the data into the pickle file
 	pickle.dump(ctt_dict, destination)
