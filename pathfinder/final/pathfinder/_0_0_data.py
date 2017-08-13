4cac50b9a9e0f9f2c395a1b77cfdb65114d0a6a7
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-08-13 19:59:26 +0100

    cleaning up

--- pathfinder/final/pathfinder/_0_0_data.py
+++ pathfinder/final/pathfinder/_0_0_data.py
@@ -20,41 +20,6 @@ def get_pickle_file(file_name):
 	return python_data
 
 
-
-
-
-def get_model_data():
-	# change directory
-	os.chdir("../")
-	os.chdir("data")
-	# get shit from file
-	f = open("data.p", "rb")
-	# load the pickle file
-	model_dict = pickle.load(f)
-	# close the pickle file
-	f.close()
-	# change directory
-	os.chdir("../")
-	os.chdir("pathfinder")
-	# return
-	return model_dict
-
-
-def get_actual_model_data():
-	# change directory
-	os.chdir("../")
-	os.chdir("data")
-	# get shit from file
-	f = open("data.json")
-	json_data = json.load(f)
-	f.close()
-	# change directory
-	os.chdir("../")
-	os.chdir("pathfinder")
-	# return
-	return json_data
-
-
 if __name__ == "__main__":
 	json_data = get_actual_model_data()
 	for jpi in json_data:
