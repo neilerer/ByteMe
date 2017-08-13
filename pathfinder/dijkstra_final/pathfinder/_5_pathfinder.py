f5d925753c43c36dd03d74b2183eb87bf1fab986
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-08-13 09:48:30 +0100

    testing to reduce run time

--- pathfinder/dijkstra_final/pathfinder/_5_pathfinder.py
+++ pathfinder/dijkstra_final/pathfinder/_5_pathfinder.py
@@ -4,6 +4,8 @@ import _1_route_mapping as rm
 import _2_route_planning as rp
 import _3_route_connections as rc
 import _4_shortest_paths as sp
+import time
+import datetime
 
 
 
@@ -73,14 +75,45 @@ if __name__ == "__main__":
 	print("Loading JSON data . . .")
 	json_data = data.get_actual_model_data()
 
+	# log file
+	log_file = open("_6_log_file.txt", "a")
+
 	weekday = 0
 	time_unit = 10
 	start = 400
 	end = 807
 
-	grc_dict = rc.get_route_connections(model_dict, json_data, r_dict, weekday, time_unit, start, end)
+	# log entry
+	coder = "ojh"
+	log_entry = "This is the baseline run."
+	log_file.write(
+		"{}\n\n".format(datetime.datetime.now())
+		+
+		"Comments\n"
+		+
+		log_entry +"\n\n"
+		+
+		"Inputs\n"
+		+
+		"weekday = {}\n".format(weekday)
+		+
+		"time_unit = {}\n".format(time_unit)
+		+
+		"start = {}\n".format(start)
+		+
+		"end = {}\n\n".format(end)
+		+
+		"Performance\n")
 
+	grc_start = time.time()
+	grc_dict = rc.get_route_connections(model_dict, json_data, r_dict, weekday, time_unit, start, end)
+	grc_time = time.time() - grc_start
+	log_file.write("grc_dict took {} to generate\n".format(grc_time))
+
+	pp_dict_start = time.time()
 	pp_dict = sp.possible_paths_dictionary(grc_dict, start, end)
+	pp_dict_time = time.time() - pp_dict_start
+	log_file.write("pp_dict took {} to generate\n".format(pp_dict_time))
 	# for route_tuple in pp_dict:
 	# 	print(route_tuple)
 	# 	print(pp_dict[route_tuple])
@@ -88,16 +121,20 @@ if __name__ == "__main__":
 
 	# print(get_travel_time(json_data, 40, 6230, '00090', weekday, time_unit))
 	# print(get_travel_time(json_data, 6230, 336, '00830', weekday, time_unit))
-
+	pp_dict_modification_start = time.time()
 	ppt_dict_modification(pp_dict, json_data, weekday, time_unit)
+	pp_dict_modification_time = time.time() - pp_dict_modification_start
+	log_file.write("pp_dict_modification took {} to generate\n\n".format(pp_dict_modification_time))
+
 	# for route_tuple in pp_dict:
 	# 	print(route_tuple)
 	# 	print(pp_dict[route_tuple])
 	# 	print("")
 
 	pathfinder_dict = pathfinder(pp_dict)
+	log_file.write("Output\n")
 	for time in pathfinder_dict:
-		print(time)
-		print(pathfinder_dict[time])
+		log_file.write("{} : {}\n".format(time, pathfinder_dict[time]))
+	log_file.write("\n\n\n\n")
 
 
