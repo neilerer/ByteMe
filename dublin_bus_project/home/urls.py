5b3614a77f22fa4d5ef258ea46893d9798b4acaf
Cara Delorey <deloreyc@tcd.ie>
2017-08-20 18:11:25 +0100

    add timetable.html, modified urls.py, views.py

--- dublin_bus_project/home/urls.py
+++ dublin_bus_project/home/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns=[
     url(r'^$', views.index, name='index'), #calls index function, this is used for the 'homepage'
+    url('timetable', views.timetable, name='timetable'),  # render timetable template, with query form
+    url('select_timetable', views.get_timetable, name='get_timetable'),  # render timetable results, with query form
 ]
