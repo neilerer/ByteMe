8b7f30fc725993caa042d16e97d3ba54e840ff75
cojowy <16202597@ucdconnect.ie>
2017-08-22 19:42:15 +0100

    r_stop.pkl created to avoid creating dictionary every time

--- dublin_bus_project/home/views.py
+++ dublin_bus_project/home/views.py
@@ -80,12 +80,15 @@ def get_route(request):
 
         selected_hour=int(selected_hour)
 
+        print(selected_origin,selected_destination,selected_day,selected_hour)
         """PATHFINDER START"""
 
         with open("stop_dict.p",'rb') as stop_dict_pkl:
             stop_dict=pkl.load(stop_dict_pkl)
 
-        r_dict = rm.routes_dict(stop_dict)
+        with open("r_dict.p","rb") as r_dict_pkl:
+            r_dict = pkl.load(r_dict_pkl)
+
         sp = sd.the_shortest_path(stop_dict, r_dict, selected_day, selected_hour, selected_origin, selected_destination)
 
         print(sp)
@@ -101,75 +104,6 @@ def get_route(request):
             if point_to_point[1] not in pathfinder_dict[point_to_point[3]]:
                 pathfinder_dict[point_to_point[3]].append(point_to_point[1])
         returned_stops=pathfinder_dict
-        print(returned_stops)
-        # possibilities=[pathfinder_dict]
-        #
-        # print(possibilities)
-
-        # come back to this loop - not working
-        # possibilities=[]
-        # for near_origin in stop_coordinates[str(selected_origin)][4]:
-        #     for near_destination in stop_coordinates[str(selected_destination)][4]:
-        #
-        #         r_dict=rm.routes_dict(stop_dict)
-        #
-        #         grc_dict = rc.get_route_connections(stop_dict, ctt_dict, r_dict, selected_day, selected_hour, near_origin, near_destination)
-        #
-        #         pp_dict = sp.possible_paths_dictionary(grc_dict, near_origin, near_destination)
-        #
-        #         clean_pp_dict = sp.clean_possible_paths_dicitonary(pp_dict)
-        #
-        #         pathfinder_dict = pf.pathfinder(clean_pp_dict, ctt_dict, selected_day, selected_hour)
-        #
-        #         possibilities.append(pathfinder_dict)
-        #
-        # print(possibilities)
-        """PATHFINDER END"""
-
-        # """PATHFINDER START"""
-        # with open("data.p", 'rb') as data_pkl:
-        #     # results from every model input
-        #     pathfinder_data = pkl.load(data_pkl)
-        #
-        # pathfinder_output=[]
-        # min_time=99999999999
-        # best_journey=0
-        # for close_to_origin in stop_coordinates[str(selected_origin)][4]: # closest stops to origin
-        #     close_to_origin=int(close_to_origin)
-        #     for close_to_destination in stop_coordinates[str(selected_destination)][4]:
-        #         close_to_destination=int(close_to_destination)
-        #         try:
-        #             shortest_path_raw = pathfinder.pathfinder(selected_day, selected_hour, close_to_origin, close_to_destination, pathfinder_data)
-        #             destination_route_list = minimum_transfers.destination_routes(close_to_destination, selected_day, selected_hour, pathfinder_data)
-        #             min_tran = minimum_transfers.minimum_transfers(shortest_path_raw, destination_route_list, close_to_origin, close_to_destination, selected_day, selected_hour,pathfinder_data)
-        #             print("min_tran done")
-        #             new_pathfinder_output=minimum_transfers.output_for_django(min_tran)
-        #             total_journey_time = 0
-        #             for trip in new_pathfinder_output:
-        #                 total_journey_time += new_pathfinder_output[trip][0]
-        #             if best_journey==0:
-        #                 best_journey=new_pathfinder_output
-        #             if total_journey_time<min_time:
-        #                 min_time=total_journey_time
-        #                 best_journey=new_pathfinder_output
-        #             print("new_pathfinder done")
-        #             pathfinder_output.append(new_pathfinder_output)
-        #         except:
-        #             continue
-        # print(min_time)
-        # print(best_journey)
-        #
-        # """PATHFINDER END"""
-
-        # #journey time calc
-
-        # returned_stops={}
-        # for trip in journey_info:
-        #     jpid=trip+"001"
-        #     stops_on_jpid=stops_on_routes[jpid]
-        #     start_point=stops_on_jpid.index(str(journey_info[trip][0]))
-        #     end_point=stops_on_jpid.index(str(journey_info[trip][1]))
-        #     returned_stops[jpid]=stops_on_jpid[start_point:end_point+1]
 
         """JOURNEY COST START"""
 
